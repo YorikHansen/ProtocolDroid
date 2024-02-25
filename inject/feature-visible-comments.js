8100c52578c8a47fd997376d761cc9c271307450
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2024-02-25 21:33:06 +0100

    Fixed featureVisibleComments bug with html_block

--- inject/feature-visible-comments.js
+++ inject/feature-visible-comments.js
@@ -4,39 +4,36 @@
 
 getMardownIt().then((md) => {		
 	const proxy = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
-	const defaultInlineRenderer = md.renderer.rules.text || proxy;
 	const defaultHTMLBlockRenderer = md.renderer.rules.html_block || proxy;
 	const defaultHTMLInlineRenderer = md.renderer.rules.html_inline || proxy;
 
-	// TODO: Check this on <li>, not in text
+	const is_comment = /<!--\s*((?:[^-\s]|-[^-]|--[^>])(?:[^-]|-[^-]|--[^>])*)\s*~\s*((?:[^-\s]|-[^-]|--[^>])(?:[^-]|-[^-]|--[^>])*)\s*-->/;
 
-	text = (tokens, idx, options, env, self) => {
-		console.log(tokens[idx]);
-		if (tokens[idx].content.startsWith('[intern]')) {
-			// tokens[idx].attrSet('class', 'intern');
-			tokens[idx].content = tokens[idx].content.substring(9);
-			return `<span class="intern">${defaultInlineRenderer(tokens, idx, options, env, self)}</span>`;
-		}
-		return defaultInlineRenderer(tokens, idx, options, env, self)
-	};
-
-	// md.renderer.rules.text = cloneInto(text, md.renderer.rules, {cloneFunctions: true});
-
-	// TODO: Lines that start with a comment should not be all comments
 	html_block = (tokens, idx, options, env, self) => {
-		const is_comment = /<!--\s*(\S.*?\S|\S)\s*~\s*(\S.*?\S|\S)\s*-->/;
-		const match = tokens[idx].content.match(is_comment);
-		if (match) {
-			return `<span class="comment comment-block"><span class="comment-icon fa fa-comment fa-fw"></span><span class="comment-content">${md.utils.escapeHtml(match[1])}</span> <span class="comment-author">${md.utils.escapeHtml(match[2])}</span></span>`;
+		let content = tokens[idx].content;
+
+		if (content.search(is_comment) < 0) {
+			return defaultHTMLBlockRenderer(tokens, idx, options, env, self);
 		}
-		return defaultHTMLBlockRenderer(tokens, idx, options, env, self)
+		
+		let o = '';
+		let i = content.search(is_comment);
+		while (i >= 0) {
+			let match = content.match(is_comment);
+			o += content.slice(0, i);
+			o += `<span class="comment"><span class="comment-icon fa fa-comment fa-fw"></span><span class="comment-content">${md.utils.escapeHtml(match[1].trim())}</span> <span class="comment-author">${md.utils.escapeHtml(match[2].trim())}</span></span>`;
+			content = content.slice(i + match[0].length);
+			i = content.search(is_comment);
+		}
+		o += content;
+
+		return md.render(o);
 	};
 
 	html_inline = (tokens, idx, options, env, self) => {
-		const is_comment = /<!--\s*(\S.*?\S|\S)\s*~\s*(\S.*?\S|\S)\s*-->/;
 		const match = tokens[idx].content.match(is_comment);
-		if (match) { // md.render(match[1])
-			return `<span class="comment"><span class="comment-icon fa fa-comment fa-fw"></span><span class="comment-content">${md.utils.escapeHtml(match[1])}</span> <span class="comment-author">${md.utils.escapeHtml(match[2])}</span></span>`;
+		if (match) {
+			return `<span class="comment"><span class="comment-icon fa fa-comment fa-fw"></span><span class="comment-content">${md.utils.escapeHtml(match[1].trim())}</span> <span class="comment-author">${md.utils.escapeHtml(match[2].trim())}</span></span>`;
 		}
 		return defaultHTMLInlineRenderer(tokens, idx, options, env, self)
 	};
@@ -54,3 +51,5 @@ document.addEventListener('click', (event) => {
 		target = target.parentElement;
 	}
 });
+
+console.log('inject/feature-visible-comments.js');
