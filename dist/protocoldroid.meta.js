69a1f7ab583c83c5b01b3e3a1114f859460d1010
Yorik Timo Hansen <yorik@fs-informatik.uni-kiel.de>
2024-04-13 19:59:50 +0200

    📦️ Update script to 0.1.2

--- dist/protocoldroid.meta.js
+++ dist/protocoldroid.meta.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name ProtocolDroid
+// @name        ProtocolDroid
 // @description A client side HedgeDoc extension that helps with protocols.
-// @version 0.1.1
-// @author Yorik Hansen
-// @homepage https://github.com/YorikHansen/ProtocolDroid
-// @match https://www.fs-infmath.uni-kiel.de/codimd/*
-// @match https://codimd.fs-infmath.uni-kiel.de/*
-// @match https://md.kif.rocks/*
-// @match https://md.fachschaften.org/*
-// @connect www.fs-infmath.uni-kiel.de
+// @version     0.1.2
+// @author      Yorik Hansen
+// @homepage    https://protocoldroid.yorik.dev/
+// @match       https://www.fs-infmath.uni-kiel.de/codimd/*
+// @match       https://codimd.fs-infmath.uni-kiel.de/*
+// @match       https://md.kif.rocks/*
+// @match       https://md.fachschaften.org/*
+// @connect     www.fs-infmath.uni-kiel.de
 // @downloadURL https://github.com/YorikHansen/ProtocolDroid/raw/main/dist/protocoldroid.user.js
-// @grant GM_addStyle
-// @grant GM_getValue
-// @grant GM_setValue
-// @grant GM_xmlhttpRequest
-// @icon https://www.fs-infmath.uni-kiel.de/codimd/icons/favicon.ico
-// @namespace https://protocoldroid.yorik.dev/
-// @run-at document-body
-// @updateURL https://github.com/YorikHansen/ProtocolDroid/raw/main/dist/protocoldroid.meta.js
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_xmlhttpRequest
+// @icon        https://www.fs-infmath.uni-kiel.de/codimd/icons/favicon.ico
+// @namespace   https://protocoldroid.yorik.dev/
+// @run-at      document-body
+// @updateURL   https://github.com/YorikHansen/ProtocolDroid/raw/main/dist/protocoldroid.meta.js
 // ==/UserScript==
