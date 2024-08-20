21b4852f41810708b6e15e5054fc45fe95d21b8d
Yorik Timo Hansen <yorik@fs-informatik.uni-kiel.de>
2024-08-20 13:00:05 +0200

    📦️ Update v0.2.3

--- dist/protocoldroid.meta.js
+++ dist/protocoldroid.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ProtocolDroid
 // @description A client side HedgeDoc extension that helps with protocols.
-// @version     0.2.2
+// @version     0.2.3
 // @author      Yorik Hansen
 // @homepage    https://protocoldroid.yorik.dev/
 // @match       https://www.fs-infmath.uni-kiel.de/codimd/*
@@ -9,13 +9,24 @@
 // @match       https://md.kif.rocks/*
 // @match       https://md.fachschaften.org/*
 // @connect     www.fs-infmath.uni-kiel.de
+// @connect     protocoldroid.yorik.dev
 // @downloadURL https://github.com/YorikHansen/ProtocolDroid/raw/main/dist/protocoldroid.user.js
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
+// @grant       GM_getResourceURL
 // @icon        https://www.fs-infmath.uni-kiel.de/codimd/icons/favicon.ico
 // @namespace   https://protocoldroid.yorik.dev/
+// @resource    JetBrainsMonoRegular https://protocoldroid.yorik.dev/fonts/jetbrains-mono/JetBrainsMono-Regular.woff2
+// @resource    JetBrainsMonoBold https://protocoldroid.yorik.dev/fonts/jetbrains-mono/JetBrainsMono-Bold.woff2
+// @resource    JetBrainsMonoItalic https://protocoldroid.yorik.dev/fonts/jetbrains-mono/JetBrainsMono-Italic.woff2
+// @resource    JetBrainsMonoBoldItalic https://protocoldroid.yorik.dev/fonts/jetbrains-mono/JetBrainsMono-BoldItalic.woff2
+// @resource    FiraCodeLight https://protocoldroid.yorik.dev/fonts/fira-code/FiraCode-Light.woff2
+// @resource    FiraCodeRegular https://protocoldroid.yorik.dev/fonts/fira-code/FiraCode-Regular.woff2
+// @resource    FiraCodeMedium https://protocoldroid.yorik.dev/fonts/fira-code/FiraCode-Medium.woff2
+// @resource    FiraCodeSemiBold https://protocoldroid.yorik.dev/fonts/fira-code/FiraCode-SemiBold.woff2
+// @resource    FiraCodeBold https://protocoldroid.yorik.dev/fonts/fira-code/FiraCode-Bold.woff2
 // @run-at      document-body
 // @updateURL   https://github.com/YorikHansen/ProtocolDroid/raw/main/dist/protocoldroid.meta.js
 // ==/UserScript==
