4430fc7de73e8b0fcea2a4bd7f702c0524bb1453
Yorik Timo Hansen <yorik@fs-informatik.uni-kiel.de>
2024-10-31 11:12:42 +0100

    🐛 Fix printstyle (for real)

--- src/feature/internalSpoiler.js
+++ src/feature/internalSpoiler.js
@@ -9,57 +9,54 @@ module.exports = new Feature(
 	(_cm, md, ns) => {
 		if (Setting.get([ns, 'blur']).value) {
 			GM_addStyle(`
-				.spoiler:not(:hover):not([data-open]) {
-					filter: blur(5px);
-				}
+				@media not print {
+					.spoiler:not(:hover):not([data-open]) {
+						filter: blur(5px);
+					}
 
-				.spoiler::marker {
-					filter: blur(0px);
+					.spoiler::marker {
+						filter: blur(0px);
+					}
 				}
 			`);
 		} else {
 			GM_addStyle(`
-				.spoiler {
-					background-color: black;
-					transition: background-color 0.5s;
-				}
+				@media not print {
+					.spoiler {
+						background-color: black;
+						transition: background-color 0.5s;
+					}
 
-				.spoiler:not(:hover):not([data-open]), 
-				.spoiler:not(:hover):not([data-open]) * {
-					color: transparent !important;
-				}
+					.spoiler:not(:hover):not([data-open]), 
+					.spoiler:not(:hover):not([data-open]) * {
+						color: transparent !important;
+					}
 
-				.spoiler:hover, .spoiler[data-open] {
-					background-color: #eee !important;
-				}
-
-				.spoiler::marker {
-					color: #333 !important;
-				}
-
-				.night .spoiler, .night .spoiler[data-open] {
-					background-color: black;
-					transition: color 0.5s;
-				}
-
-				.night .spoiler:not(:hover), .night .spoiler:not(:hover) *,
-				.night .spoiler[data-open], .night .spoiler[data-open] * {
-					color: transparent !important;
-				}
-
-				.night .spoiler:hover,
-				.night .spoiler[data-open] {
-					background-color: black !important;
-				}
-
-				.night .spoiler::marker {
-					color: #ededed !important;
-				}
-
-				@media print {
-					.spoiler, .spoiler * {
-						color: black !important;
+					.spoiler:hover, .spoiler[data-open] {
 						background-color: #eee !important;
+					}
+
+					.spoiler::marker {
+						color: #333 !important;
+					}
+
+					.night .spoiler, .night .spoiler[data-open] {
+						background-color: black;
+						transition: color 0.5s;
+					}
+
+					.night .spoiler:not(:hover), .night .spoiler:not(:hover) *,
+					.night .spoiler[data-open], .night .spoiler[data-open] * {
+						color: transparent !important;
+					}
+
+					.night .spoiler:hover,
+					.night .spoiler[data-open] {
+						background-color: black !important;
+					}
+
+					.night .spoiler::marker {
+						color: #ededed !important;
 					}
 				}
 			`);
@@ -67,15 +64,19 @@ module.exports = new Feature(
 
 		const proxy = (tokens, idx, options, _env, self) =>
 			self.renderToken(tokens, idx, options);
-		const defaultListItemOpenRenderer = md.renderer.rules.list_item_open || proxy;
+		const defaultListItemOpenRenderer =
+			md.renderer.rules.list_item_open || proxy;
 		// (?:[^-\n]|-[^-\n]|--[^>\n])*
 		const isInternal = /^\[(?:intern(?:al)?|spoiler)\]\s*(\S.*)$/i;
 
-	
 		md.renderer.rules.list_item_open = (tokens, idx, options, env, self) => {
-
 			// list
-			if (tokens[idx + 1] && tokens[idx + 1].type === 'paragraph_open' && tokens[idx + 2] && tokens[idx + 2].type === 'inline') {
+			if (
+				tokens[idx + 1] &&
+				tokens[idx + 1].type === 'paragraph_open' &&
+				tokens[idx + 2] &&
+				tokens[idx + 2].type === 'inline'
+			) {
 				let list_item_open = tokens[idx];
 				let inline = tokens[idx + 2];
 
@@ -101,7 +102,5 @@ module.exports = new Feature(
 			}
 		});
 	},
-	[
-		new BooleanSetting('blur', 'Blur Spoilers', false),
-	],
+	[new BooleanSetting('blur', 'Blur Spoilers', false)],
 ).setDescription('Highlight TODO notes in the editor');
